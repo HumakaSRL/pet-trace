6660467ee4dc47dcfe882322662ab0066037e153
Humaka01 <Imad_Karaki@yahoo.com>
2024-11-11 01:10:39 +0200

    updated navbar across pages

--- site/public/assets/js/navbar.js
+++ site/public/assets/js/navbar.js
@@ -1,23 +1,36 @@
 firebase.auth().onAuthStateChanged(async (user) => {
     if (user) {
+        // Show logout button and hide login button
         loginButton.style.display = "none";
         logoutButton.style.display = "block";
+
+        // Check if user has a display name and set it
+        if (user.displayName) {
+            usernameNavbarSpan.textContent = user.displayName;
+            userGreeting.style.display = "flex"; // Show greeting message
+        }
     } else {
+        // Show login button and hide logout button
         loginButton.style.display = "block";
         logoutButton.style.display = "none";
     }
 });
 
-logoutButton.addEventListener("click", () => {
-    signOut()
+document.addEventListener("DOMContentLoaded", () => {
+    logoutButton.addEventListener("click", logout);
+    loginButton.addEventListener("click", () => {
+        window.location = "/login.html";
+    });
+});
+
+function logout() {
+    firebase
+        .auth()
+        .signOut()
         .then(() => {
-            location.reload();
+            location.reload(); // Reload the page after sign out
         })
         .catch((error) => {
             console.error("Error signing out: ", error);
         });
-});
-
-loginButton.addEventListener("click", () => {
-    window.location = "/login.html";
-});
+}
