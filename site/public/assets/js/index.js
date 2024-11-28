929772dd7397975fceef53f33d90e63548ebc907
Humaka01 <Imad_Karaki@yahoo.com>
2024-11-28 03:54:09 +0200

    checkpoint

--- site/public/assets/js/index.js
+++ site/public/assets/js/index.js
@@ -1,12 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
     searchButton.addEventListener("click", (e) => {
         e.preventDefault();
-        searchPet(microchipIdInput);
+
+        // Get the microchip ID input value
+        let microchipId = microchipIdInput.value.trim();
+
+        // Validate the microchip ID
+        const isValid = /^[a-zA-Z0-9]{9,15}$/.test(microchipId);
+
+        if (!isValid) {
+            errorContainer.textContent =
+                "Invalid ID. Ensure it has 9-15 characters, only letters and numbers, without dashes.";
+            return;
+        }
+
+        searchPet(microchipId.toUpperCase());
     });
 });
 
-function searchPet(inputElement) {
-    const chip = inputElement.value.trim();
+function searchPet(chip) {
     if (chip) window.location.href = `/view.html?chip=${chip}`;
     else alert("Please enter a valid Microchip ID.");
 }
